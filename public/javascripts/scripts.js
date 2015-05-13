5b3df89893da7597454429b4ddc4f32e2936723e
Michelle C. Funk <michelle@michellefunk.com>
2015-05-13 10:43:02 -0500

    Add delete function

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,4 +1,29 @@
 $(document).ready(function() {
+    populatePage();
+
+    $('.main').on("click", ".delete", function() {
+        console.log($(this));
+        var assignID = $(this).data('id');
+        $('.main').empty();
+        $.ajax({
+            type: 'DELETE',
+            datatype: 'json',
+            url: "/assignment/" + assignID,
+            complete: function(){
+            console.log("Ajax delete call complete");
+            },
+            success: function(post){
+                console.log("delete successful: ", post);
+            },
+            error: function(xhr, err){
+                console.log("delete error: ", err);
+            }
+        });
+        populatePage();
+    });
+});
+
+function populatePage() {
     $.ajax({
         type: 'GET',
         datatype: 'json',
@@ -10,15 +35,15 @@ $(document).ready(function() {
             console.log(data);
             for (var app in data) {
                 var foo = "app" + app;
-                $('.main').append("<div id='" + foo + "'></div>");
+                $('.main').append("<div class='thisAssignment' id='" + foo + "'></div>");
                 $("#"+foo).append(data[app]['_id'] + '</br>');
                 $("#"+foo).append(data[app]['name'] + '</br>');
                 $("#"+foo).append(data[app]['score'] + '</br>');
-                //$("#"+foo).append("Button stuff will go here");
+                $("#"+foo).append("<button class='delete' id='delete"+ app + "' data-id='" + data[app]['_id'] + "'>Delete</button>");
             }
         },
         error: function(xhr, status){
             console.log("Error : ", xhr, " ", status);
         }
     });
-});
+}
